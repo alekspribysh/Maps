c1df272821250656417194f5e0c862a9b9c32415
alekspribysh <alekspribysh@gmail.com>
2015-08-05 17:16:46 -0700

    Initial commit

--- src/test/java/com/SideMenu/SideMenu.java
+++ src/test/java/com/SideMenu/SideMenu.java
@@ -24,6 +24,13 @@ public class SideMenu {
 
 
     public AndroidDriver driver;
+    public void getSideMenu(){
+        TouchAction action0 = new TouchAction(driver).longPress(50, 1000, 3000).moveTo(1000, 1000);
+        action0.release().perform();
+
+
+    }
+
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
@@ -40,12 +47,46 @@ public class SideMenu {
 
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("com.google.android.apps.gmm:id/watermark_image")));
-        System.out.println("Passed!!!!!!!");
-        TouchAction action0 = new TouchAction(driver).longPress(50, 1000, 3000).moveTo(1000, 1000);
-        action0.release().perform();
+        getSideMenu();
         Assert.assertNotNull( driver.findElement(By.id("com.google.android.apps.gmm:id/layers_scrollview")));
 
     }
+
+    @Test
+
+    public void trafficLayer () throws MalformedURLException {
+        getSideMenu();
+        driver.findElement(By.id("com.google.android.apps.gmm:id/traffic_button")).click();
+    }
+
+
+    @Test
+
+    public void publicLayer () throws MalformedURLException {
+        getSideMenu();
+        driver.findElement(By.id("com.google.android.apps.gmm:id/transit_button")).click();
+    }
+
+
+    @Test
+
+    public void tips (){
+        driver.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]" +
+                "/android.widget.LinearLayout[1]/android.view.View[1]/android.widget.FrameLayout[3]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]" +
+                "/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.ImageView[1]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("com.google.android.apps.gmm:id/layers_scrollview")));
+        TouchAction action0 = new TouchAction(driver).longPress(620, 1620, 3000).moveTo(620, 800);
+        action0.release().perform();
+        Assert.assertEquals(driver.findElementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.support.v4.widget.DrawerLayout[1]" +
+                "/android.widget.LinearLayout[2]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]" +
+                "/android.widget.TextView[1]").getText().toString(), "Tips & tricks");
+
+    }
+
+
+
+
 
 
 
